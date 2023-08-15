c94891c8e997e2a9dca3f981934f83dfc3cfefc8
Gabriel Godoy <gabyreload@gmail.com>
2023-08-15 14:32:51 -0300

    Chnage createConnection for createPool connection

--- src/dao/dbMySqlConfig.js
+++ src/dao/dbMySqlConfig.js
@@ -1,9 +1,11 @@
 import mysql from "mysql";
 import config from "../config/config.js";
 
-export const connectionUrbano = mysql.createConnection({
+export const connectionUrbano = mysql.createPool({
+  connectionLimit: 100,
   host: config.sql_host,
   user: config.sql_user,
   password: config.sql_password,
   database: config.sql_database,
+  debug: false,
 });
