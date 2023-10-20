c3191ff09b0183cd9888d3dd25fe93a397e005e5
Gabriel Godoy <gabyreload@gmail.com>
2023-10-20 15:42:29 -0300

    env files config

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV === "production") dotenv.config();
+
+if (process.env.NODE_ENV !== "production")
+  dotenv.config({ path: `${process.env.PWD}/.env.${process.env.NODE_ENV}` });
 
 export default {
   port: process.env.PORT,
