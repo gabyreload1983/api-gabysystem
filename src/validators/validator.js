e650d4ca44321ade85db4f760de56e089039dcc3
Gabriel Godoy <gabyreload@gmail.com>
2024-06-26 12:10:17 -0300

    isValidOrder was added in orders controller

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -1,1 +1,22 @@
 export const incompleteValues = (...values) => values.some((value) => !value);
+
+export const isValidOrder = (order) => {
+  const requiredKeys = [
+    "code",
+    "client",
+    "phone",
+    "mail",
+    "sector",
+    "description",
+    "serie",
+    "fail",
+    "accesories",
+    "priority",
+  ];
+
+  const missingKeys = requiredKeys.filter(
+    (requiredKey) => order.hasOwnProperty(requiredKey) === false
+  );
+
+  return missingKeys.length ? false : true;
+};
