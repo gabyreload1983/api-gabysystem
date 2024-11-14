0285e5052ff98dc5313b9f1e5cf4c422f26d499f
Gabriel Godoy <gabyreload@gmail.com>
2024-11-14 14:08:22 -0300

    start endpoint replacements

---
+++ src/dao/mongoManagers/models/replacements.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const replacementsCollection = "replacements";
+
+const replacementsSchema = new mongoose.Schema(
+  {
+    orderNumber: {
+      type: Number,
+    },
+    technical_code: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    images: {
+      type: String,
+    },
+    supplier_code: {
+      type: String,
+    },
+    supplier: {
+      type: String,
+    },
+    cost: {
+      type: Number,
+    },
+    finalPrice: {
+      type: Number,
+    },
+    delay: {
+      type: Number,
+    },
+    shipmment: {
+      type: String,
+    },
+    customerConfirmation: {
+      type: Boolean,
+    },
+    status: {
+      type: String,
+    },
+    linkSupplier: {
+      type: String,
+    },
+    notes: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const replacementsModel = mongoose.model(
+  replacementsCollection,
+  replacementsSchema
+);
+
+export default replacementsModel;
