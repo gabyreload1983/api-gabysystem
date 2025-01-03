bd603761d42806938a5d1a2745b47030fba7beb9
Gabriel Godoy <gabyreload@gmail.com>
2025-01-03 09:54:26 -0300

    replace null for empty string

--- src/dao/DTOs/ReplacementCreate.dto.js
+++ src/dao/DTOs/ReplacementCreate.dto.js
@@ -1,18 +1,18 @@
 export default class ReplacementCreateDto {
   constructor(replacement) {
-    this.orderNumber = replacement?.orderNumber || null;
+    this.orderNumber = replacement?.orderNumber || "";
     this.technical_code = replacement.technical_code;
     this.description = replacement.description;
-    this.images = replacement?.images || null;
-    this.supplier_code = replacement?.supplier_code || null;
-    this.supplier = replacement?.supplier || null;
+    this.images = replacement?.images || "";
+    this.supplier_code = replacement?.supplier_code || "";
+    this.supplier = replacement?.supplier || "";
     this.cost = replacement?.cost || 0;
     this.finalPrice = replacement?.finalPrice || 0;
     this.delay = replacement?.delay || 0;
-    this.shipmment = replacement?.shipmment || null;
+    this.shipmment = replacement?.shipmment || "";
     this.customerConfirmation = replacement?.customerConfirmation || false;
-    this.status = replacement?.status || null;
-    this.linkSupplier = replacement?.linkSupplier || null;
-    this.notes = replacement?.notes || null;
+    this.status = replacement?.status || "";
+    this.linkSupplier = replacement?.linkSupplier || "";
+    this.notes = replacement?.notes || "";
   }
 }
